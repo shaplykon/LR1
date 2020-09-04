2580197db33f26fa1bf4291c5a09e604ab87db64
shaplykon <shaplykon@gmail.com>
2020-09-05 00:10:57 +0300

    Changed

--- app/src/main/java/com/example/lr1/MainActivity.java
+++ app/src/main/java/com/example/lr1/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.lr1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.pm.PackageInfo;
 import android.os.Bundle;
-
+import android.widget.TextView;
+import com.example.lr1.BuildConfig;
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        pff
+        TextView versionNameTextView = (TextView)findViewById(R.id.versionNameTextView);
+        int versionCode = BuildConfig.VERSION_CODE;
+        String versionName = BuildConfig.VERSION_NAME;
+        versionNameTextView.setText(versionName);
     }
 }
